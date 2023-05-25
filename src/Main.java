c57e2d9ba35dea6beae1a91c73cf9cbc493b2e8d
DEVSI30 <offset654@gmail.com>
2023-05-25 20:14:48 +0900

    코딩테스트 연습 > level2 > 영어 끝말잇기

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,10 @@ import programmers.*;
 
 public class Main {
     public static void main(String[] args) {
-        Solution_045_12973 solution= new Solution_045_12973();
-        int solution1 = solution.solution("baabaa");
-        System.out.println("solution1 = " + solution1);
+        Solution_046_12981 solution= new Solution_046_12981();
+        int[] solution1 = solution.solution(2, new String[]{"tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"});
+        System.out.println("solution1 = " + solution1[0]);
+        System.out.println("solution1 = " + solution1[1]);
     }
 }
 
---
+++ src/programmers/Solution_046_12981.java
@@ -0,0 +1,38 @@
+package programmers;
+
+import java.util.HashSet;
+
+public class Solution_046_12981 {
+    public int[] solution(int n, String[] words) {
+        int[] answer = {0, 0};
+        // 중복되거나 이어지지 않는 단어의 위치를 찾고
+        // n 으로 적당히 누가 몇 번째에서 실패했는지 계산한다.
+        HashSet<String> usedWords = new HashSet<>();
+        String lastWord = "";
+        int count = 0;
+        for (String word : words) {
+            count++;
+            if (lastWord.isEmpty()) {
+                lastWord = word;
+                usedWords.add(word);
+                continue;
+            }
+
+            if (lastWord.charAt(lastWord.length() - 1) != word.charAt(0) || usedWords.contains(word)) {
+                break;
+            }
+
+            lastWord = word;
+            usedWords.add(word);
+        }
+
+        if (count == words.length && count == usedWords.size()) {
+            return answer;
+        }
+
+        answer[0] = (count-1) % n + 1;
+        answer[1] = (count-1) / n + 1;
+
+        return answer;
+    }
+}
