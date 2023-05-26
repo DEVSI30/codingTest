058888ff0137dfb0ec4620dd7c3ecebf5e2c8984
devsi30 <offset654@gmail.com>
2023-05-26 20:56:26 +0900

    코딩테스트 연습 > level2 > 구명보트

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,9 @@ import programmers.*;
 
 public class Main {
     public static void main(String[] args) {
-        Solution_047_42842 solution= new Solution_047_42842();
-        int[] solution1 = solution.solution(10, 2);
-        System.out.println("solution1 = " + solution1[0]);
-        System.out.println("solution1 = " + solution1[1]);
+        Solution_048_42885 solution= new Solution_048_42885();
+        int solution1 = solution.solution(new int[]{70, 50, 80, 50}, 100);
+        System.out.println("solution1 = " + solution1);
     }
 }
 
