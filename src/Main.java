cbed8ac95dd26912dc2ae87363f9f1070666aac5
DEVSI30 <offset654@gmail.com>
2023-06-17 17:19:16 +0900

    코딩테스트 연습 > level2 > 할인 행사

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,11 @@ import programmers.*;
 
 public class Main {
     public static void main(String[] args) {
-        Solution_059_64065 solution = new Solution_059_64065();
-        int[] solution1 = solution.solution("{{2},{2,1},{2,1,3},{2,1,3,4}}");
-        for (int i : solution1) {
-            System.out.println("i = " + i);
-        }
+        Solution_060_131127 solution = new Solution_060_131127();
+        int solution1 = solution.solution(new String[]{"banana", "apple", "rice", "pork", "pot"},
+                new int[]{3, 2, 2, 2, 1},
+                new String[]{"chicken", "apple", "apple", "banana", "rice", "apple", "pork", "banana", "pork", "rice", "pot", "banana", "apple", "banana"});
+        System.out.println("solution1 = " + solution1);
     }
 }
 
---
+++ src/programmers/Solution_060_131127.java
@@ -0,0 +1,49 @@
+package programmers;
+
+import java.util.ArrayDeque;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Queue;
+
+public class Solution_060_131127 {
+    public int solution(String[] want, int[] number, String[] discount) {
+        int answer = 0;
+        Map<String, Integer> wantMap = new HashMap<>();
+
+        for (int i = 0; i < want.length; i++) {
+            wantMap.put(want[i], number[i]);
+        }
+
+        Map<String, Integer> discountMap = new HashMap<>();
+        Queue<String> discountHistory = new ArrayDeque<>();
+        for (String d : discount) {
+            discountMap.putIfAbsent(d, 0);
+            discountMap.put(d, discountMap.get(d) + 1);
+
+            discountHistory.add(d);
+            if (discountHistory.size() == 11) {
+                String poll = discountHistory.poll();
+                discountMap.put(poll, discountMap.get(poll) - 1);
+            }
+
+            if (matchMap(wantMap, discountMap)) {
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+
+    public boolean matchMap(Map<String, Integer> m1, Map<String, Integer> m2) {
+        // m1 이 기준
+        for (String k1 : m1.keySet()) {
+            if (!m2.containsKey(k1)) {
+                return false;
+            }
+            if (m1.get(k1) != m2.get(k1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
