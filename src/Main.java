539bb404ee268d732eff3100cce292f036848225
devsi30 <offset654@gmail.com>
2023-08-02 04:01:45 +0900

    코딩테스트 연습 > level2 > 쿼드압축 후 개수 세기

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,10 @@ import programmers.*;
 
 public class Main {
     public static void main(String[] args) {
-        Solution_080_42839 solution = new Solution_080_42839();
-        int result = solution.solution("011");
-        System.out.println("result = " + result);
+        Solution_081_68936 solution = new Solution_081_68936();
+        int[] result = solution.solution(new int[][]{{1, 1, 0, 0}, {1, 0, 0, 0}, {1, 0, 0, 1}, {1, 1, 1, 1}});
+        System.out.println("result = " + result[0]);
+        System.out.println("result = " + result[1]);
     }
 }
 
---
+++ src/programmers/Solution_081_68936.java
@@ -0,0 +1,92 @@
+package programmers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution_081_68936 {
+    public int[] solution(int[][] arr) {
+        List<Array2D> array2DList = new ArrayList<>();
+        array2DList.add(new Array2D(arr));
+
+        int n = arr.length / 2;
+        while (n > 0) {
+            Array2D array2D = new Array2D(n, n);
+            array2D.fill(-1);
+            array2DList.add(array2D);
+            n /= 2;
+        }
+
+        for (int i = 0; i < array2DList.size() - 1; i++) {
+            Array2D prev = array2DList.get(i);
+            Array2D next = array2DList.get(i + 1);
+            for (int y = 0; y < prev.height; y += 2) {
+                for (int x = 0; x < prev.width; x += 2) {
+                    arr = prev.arr;
+                    // 이전 값 중 -1 이 한 개라도 있으면 압축이 안 됨
+                    if (arr[y][x] == -1 || arr[y + 1][x] == -1 || arr[y][x + 1] == -1 || arr[y + 1][x + 1] == -1) {
+                        continue;
+                    }
+                    // 4개의 값이 같으면 다음 결과에 압축 값을 저장하고 이전 값은 -1로 채운다.
+                    if (arr[y][x] == arr[y + 1][x] && arr[y][x] == arr[y + 1][x + 1] && arr[y][x] == arr[y][x + 1]) {
+                        next.arr[y / 2][x / 2] = arr[y][x];
+                        arr[y][x] = -1;
+                        arr[y + 1][x] = -1;
+                        arr[y][x + 1] = -1;
+                        arr[y + 1][x + 1] = -1;
+                    }
+                }
+            }
+        }
+
+        return array2DList.stream().map(x -> count0And1(x)).reduce((x, y) -> new int[]{x[0] + y[0], x[1] + y[1]}).get();
+    }
+
+    public int[] count0And1(Array2D arr) {
+        int count0 = 0;
+        int count1 = 0;
+        for (int y = 0; y < arr.height; y++) {
+            for (int x = 0; x < arr.width; x++) {
+                if (arr.arr[y][x] == 0) {
+                    count0++;
+                } else if (arr.arr[y][x] == 1) {
+                    count1++;
+                }
+            }
+        }
+        return new int[]{count0, count1};
+    }
+
+    class Array2D {
+        int[][] arr;
+        int height;
+        int width;
+
+        public Array2D(int width, int height) {
+            arr = new int[height][width];
+            this.height = height;
+            this.width = width;
+        }
+
+        public Array2D(int[][] o) {
+            this.height = o.length;
+            if (height == 0) {
+                throw new RuntimeException("Width can't be 0");
+            }
+            this.width = o[0].length;
+            arr = new int[height][width];
+            for (int y = 0; y < height; y++) {
+                for (int x = 0; x < width; x++) {
+                    arr[y][x] = o[y][x];
+                }
+            }
+        }
+
+        public void fill(int value) {
+            for (int y = 0; y < height; y++) {
+                for (int x = 0; x < width; x++) {
+                    arr[y][x] = value;
+                }
+            }
+        }
+    }
+}
