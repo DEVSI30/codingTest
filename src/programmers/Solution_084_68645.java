1dddd19924a27020b94b09c7a655f5987331d860
devsi30 <offset654@gmail.com>
2023-08-07 20:59:13 +0900

    코딩테스트 연습 > level2 > 삼각 달팽이 - 계차수열을 통해서 offset을 구한다.

--- src/programmers/Solution_084_68645.java
+++ src/programmers/Solution_084_68645.java
@@ -3,15 +3,6 @@ package programmers;
 public class Solution_084_68645 {
     public int[] solution(int n) {
         int[] answer = new int[n * (n + 1) / 2];
-        int[] startValueByGroup = new int[(n - 1) / 3 + 1];
-        for (int c = 0; c < startValueByGroup.length; c++) {
-            if (c == 0) {
-                startValueByGroup[c] = 1;
-            } else {
-                startValueByGroup[c] = startValueByGroup[c - 1] + 3 * (n - 3 * c + 2);
-            }
-        }
-
         int index = 0;
         for (int y = 0; y < n; y++) {
             for (int x = 0; x <= y; x++) {
@@ -23,7 +14,7 @@ public class Solution_084_68645 {
                 int nowX = x - group;
                 int nowY = y - group * 2;
                 int nowN = n - 3 * group;
-                int offset = startValueByGroup[group];
+                int offset = (group + 1) * group / 2 * -9 + (3 * n + 6) * group + 1;
                 answer[index++] = (nowY == nowN - 1 || nowX == 0) ? nowY + nowX + offset : 3 * (nowN - 1) - nowY + offset;
             }
         }
