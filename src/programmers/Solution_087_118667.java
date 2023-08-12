867d9cc75ce6e6ccf31fd534ebfcb69ae3a6112f
devsi30 <offset654@gmail.com>
2023-08-12 11:27:20 +0900

    코딩테스트 연습 > level2 > 두 큐 합 같게 만들기 - Greedy 로 풀면 된다는 해답 보고나서 작성

--- src/programmers/Solution_087_118667.java
+++ src/programmers/Solution_087_118667.java
@@ -1,61 +1,48 @@
 package programmers;
 
-import java.util.ArrayDeque;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Solution_087_118667 {
-    private final int[] p1 = {0, 1};
-    private final int[] p2 = {1, 0};
-
     public int solution(int[] queue1, int[] queue2) {
-        ArrayDeque<Status> statuses = new ArrayDeque<>();
-        int lengthSum = queue1.length + queue2.length;
-        boolean[][] visited = new boolean[lengthSum][lengthSum];
-        int[] queueConcat = new int[lengthSum];
-        System.arraycopy(queue1, 0, queueConcat, 0, queue1.length);
-        System.arraycopy(queue2, 0, queueConcat, queue1.length, queue2.length);
+        int answer = 0;
+        int totalLength = queue1.length + queue2.length;
+        ArrayDeque<Integer> q1 = new ArrayDeque<>(Arrays.stream(queue1).boxed().collect(Collectors.toList()));
+        ArrayDeque<Integer> q2 = new ArrayDeque<>(Arrays.stream(queue2).boxed().collect(Collectors.toList()));
 
         long q1Sum = getSum(queue1);
         long q2Sum = getSum(queue2);
+        long totalSum = q1Sum + q2Sum;
 
+        // 두 큐의 합이 같다면 바로 종료
         if (q1Sum == q2Sum) {
             return 0;
         }
 
-        statuses.add(new Status(0, 0, q1Sum, q2Sum));
-
-        while (!statuses.isEmpty()) {
-            Status now = statuses.pollFirst();
-
-            for (int i = 0; i < 2; i++) {
-                int nextQ1Count = now.q1popCount + p1[i];
-                int nextQ2Count = now.q2popCount + p2[i];
-                int indexQ1 = (now.q1popCount) % queueConcat.length;
-                int indexQ2 = (now.q2popCount + queue1.length) % queueConcat.length;
-                int nextQ1Index = nextQ1Count;
-                int nextQ2Index = nextQ2Count;
-
-                if (nextQ1Index == queueConcat.length || nextQ2Index == queueConcat.length) {
-                    continue;
-                }
-
-                if (visited[nextQ1Index][nextQ2Index]) {
-                    continue;
-                }
-                int q1PopValue = queueConcat[indexQ1];
-                int q2PopValue = queueConcat[indexQ2];
-                long nextQ1Sum = now.q1Sum - p1[i] * q1PopValue + p2[i] * q2PopValue;
-                long nextQ2Sum = now.q2Sum - p2[i] * q2PopValue + p1[i] * q1PopValue;
-
-                if (nextQ1Sum == nextQ2Sum) {
-                    return now.q1popCount + now.q2popCount + 1;
-                }
-
-                visited[nextQ1Index][nextQ2Index] = true;
-                statuses.addLast(new Status(nextQ1Count, nextQ2Count, nextQ1Sum, nextQ2Sum));
-            }
+        // 두 큐의 합이 같다는 건 각각 합이 N, N 이 되어야 같아지는데 그 합은 2N, 즉 짝수이다.
+        if (totalSum % 2 == 1) {
+            return -1;
         }
 
-        return -1;
+        while (q1Sum != q2Sum) {
+            // 큰 쪽에서 작은 쪽으로 이동
+            if (q1Sum > q2Sum) {
+                Integer polled = q1.pollFirst();
+                q2.addLast(polled);
+                q1Sum -= polled;
+                q2Sum += polled;
+            } else if (q1Sum < q2Sum) {
+                Integer polled = q2.pollFirst();
+                q1.addLast(polled);
+                q1Sum += polled;
+                q2Sum -= polled;
+            }
+            answer++;
+            if (answer == 2 * totalLength) {
+                return -1;
+            }
+        }
+        return answer;
     }
 
     public long getSum(int[] queue) {
@@ -64,20 +51,5 @@ public class Solution_087_118667 {
             result += i;
         }
         return result;
-    }
-
-    public class Status {
-        private int q1popCount;
-        private int q2popCount;
-
-        private long q1Sum;
-        private long q2Sum;
-
-        public Status(int q1popCount, int q2popCount, long q1Sum, long q2Sum) {
-            this.q1popCount = q1popCount;
-            this.q2popCount = q2popCount;
-            this.q1Sum = q1Sum;
-            this.q2Sum = q2Sum;
-        }
     }
 }
